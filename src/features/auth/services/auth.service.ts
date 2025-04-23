17ef856f5e4732ea937e01300ccfd62e85c64bc6
anastasiiaskreka <skreka.nastya@gmail.com>
2025-04-23 11:29:58 +0300

    feat: Add google oauth

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -15,6 +15,14 @@ class AuthService {
         return response
     }
 
+    public async oauthByProvider(provider: 'google') {
+        const response = await api.get<{url: string}>(
+            `auth/oauth/connect/${provider}`
+        )
+
+        return response
+    }
+
     public async logout() {
         const response = await api.post('auth/logout')
 
---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export default function middleware(request: NextResponse) {
+ const {url, cookies} = request
+
+ const session = cookies.get('session')?.value
+
+ const isAuthPage = url.includes('/auth')
+
+ if(isAuthPage) {
+    if (session) {
+      return NextResponse.redirect(new URL('/dashboard/settings', url))
+    }
+
+    return NextResponse.next()
+ }
+
+ if (!session) {
+  return NextResponse.redirect(new URL('/auth/login', url))
+ }
+
+
+}
+
+export const config = {
+    matcher: ['/auth/:path*', '/dashboard/:path*']
+}
