56faa56642e115af7f643df6488ff26e39447f02
anastasiiaskreka <skreka.nastya@gmail.com>
2025-04-28 13:50:13 +0300

    fix: Change bg color

--- src/features/auth/components/AuthWrapper.tsx
+++ src/features/auth/components/AuthWrapper.tsx
@@ -21,7 +21,7 @@ export function AuthWrapper({
 }: PropsWithChildren<AuthWrapperProps>) {
     return (
         <Card className='w-[400px]'>
-            <CardHeader className='space-y-2'>
+            <CardHeader className='space-y-2 bg-amber-500'>
                 <CardTitle>{heading}</CardTitle>
                 {description && (
                     <CardDescription>{description}</CardDescription>
